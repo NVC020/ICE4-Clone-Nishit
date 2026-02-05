83b14ca76149a8ad9a79976e8e2d7adf9463e52a
NVC020 <changeln@sheridancollege.ca>
2026-02-05 18:13:29 -0500

    Initial commit

---
+++ src/main/java/ca/sheridan/nishit/projectnishit/ProjectNishit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridan.nishit.projectnishit;
+
+/**
+ *
+ * @author nishi
+ */
+public class ProjectNishit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
